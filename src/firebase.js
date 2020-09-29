bfbba7e5ad119a6e693eaa78c610d45118007501
jinyongp <dev.jinyongp@gmail.com>
2020-09-29 22:43:39 +0900

    🔧 Initialize firebase app

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+// React에서 env를 사용할 때 이름은 REACT_APP으로 시작해야 한다.
+// 아래 API key는 github으로부터 숨긴 것 뿐 빌드 시 사용자들에게 공개될 수 있다.
+// 특정 도메인에서만 사용할 수 있도록 하는 등 보안을 강화할 수 있다.
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebaseConfig from './firebase';
+import firebase from 'firebase/app';
 import App from './App';
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
