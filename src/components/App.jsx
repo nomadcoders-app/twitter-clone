9771f0c186c565b969c666b9feb6efa09ec242ff
jinyongp <dev.jinyongp@gmail.com>
2020-10-01 19:23:31 +0900

    ✨ Add SignIn / SignUp features

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { auth } from '../firebase';
 import Router from './Router';
 
 const App = () => {
-  const [user, setUser] = useState(auth.currentUser);
-  return <Router user={user} />;
+  const [user, setUser] = useState(null);
+  const [init, setInit] = useState(false);
+  useEffect(() => {
+    // user의 상태 변환에 따른 이벤트를 정의한다.
+    auth.onAuthStateChanged((_user) => {
+      setUser(_user);
+      setInit(true);
+    });
+  }, []);
+  return (
+    <>
+      {init ? <Router user={user} /> : <div>Initializing...</div>}
+      <footer>
+        {`© ${new Date().getFullYear()} Nwitter`}
+      </footer>
+    </>
+  );
 };
 
 export default App;
--- src/routes/Auth.jsx
+++ src/routes/Auth.jsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../firebase';
 
-const Auth = () => (
-  <div>
-    Auth Route
-  </div>
-);
+const Auth = () => {
+  const [input, setInput] = useState({
+    email: '',
+    password: '',
+  });
+  const [errorMessage, setErrorMessage] = useState(' ');
+  const [newAccount, setNewAccount] = useState(true);
+
+  const onChange = (event) => {
+    event.preventDefault();
+    const { target: { name, value } } = event;
+    setInput({ ...input, [name]: value });
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const { email, password } = input;
+    try {
+      let userCredential;
+      if (newAccount) {
+        userCredential = await auth.createUserWithEmailAndPassword(email, password);
+        console.log(userCredential);
+      } else {
+        userCredential = await auth.signInWithEmailAndPassword(email, password);
+      }
+      const { user } = userCredential;
+    } catch (error) {
+      const { message } = error;
+      setErrorMessage(message);
+    }
+  };
+
+  const toggleNewAccount = () => setNewAccount((prev) => !prev);
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="email"
+          placeholder="Email"
+          onChange={onChange}
+          value={input.email}
+          pattern="^\w+((\.\w+)?)+@\w+.?\w+\.\w+$"
+          required
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          onChange={onChange}
+          value={input.password}
+          pattern=".{6,}"
+          required
+        />
+        <input
+          type="submit"
+          value={newAccount ? 'Sign Up' : 'Sign In'}
+        />
+        <span onClick={toggleNewAccount} aria-hidden>
+          {newAccount ? 'Sign In' : 'Sign Up'}
+        </span>
+      </form>
+      <input type="button" value="Continue with Google" />
+      <input type="button" value="Continue with Github" />
+      {errorMessage}
+    </div>
+  );
+};
 
 export default Auth;
